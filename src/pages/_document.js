b1171d8fbaae94be8a0974d3a2d481ae917648cb
CedricMurairi <murairicedric@gmail.com>
2023-07-12 10:34:47 +0200

    meta tags

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,16 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="description" content="Infinitloop" />
+        <meta name="keywords" content="Infinitloop" />
+        <meta name="author" content="Cedric Murairi" />
+        <meta name="theme-color" content="#000000" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="robots" content="INDEX, FOLLOW" />
+        <meta name="googlebot" content="INDEX, FOLLOW" />
+        <meta name="google" content="notranslate" />
+      </Head>
       <body>
         <Main />
         <NextScript />
